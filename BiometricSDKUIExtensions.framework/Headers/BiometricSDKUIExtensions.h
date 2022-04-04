615cbcd3e50f6da7aead2ea97e1987b513c86cc9
ZYTRUST SA <ztmobile@zytrust.com>
2022-04-04 10:00:14 -0500

    version 1.5.3
    version 1.5.3

---
+++ BiometricSDKUIExtensions.framework/Headers/BiometricSDKUIExtensions.h
@@ -0,0 +1,19 @@
+//
+//  BiometricSDKUIExtensions.h
+//  BiometricSDKUIExtensions
+//
+//  Created by Leszek Szary on 26.09.2018.
+//  Copyright © 2018 MORPHO. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BiometricSDKUIExtensions.
+FOUNDATION_EXPORT double BiometricSDKUIExtensionsVersionNumber;
+
+//! Project version string for BiometricSDKUIExtensions.
+FOUNDATION_EXPORT const unsigned char BiometricSDKUIExtensionsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BiometricSDKUIExtensions/PublicHeader.h>
+
+
